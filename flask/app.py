70f71dd49427a627d12e863c5e2f2c2a7442b719
Hasnat Bashir <hasnat@kscope.ai>
2025-05-29 14:27:53 +0500

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -8,6 +8,7 @@ db = SQLAlchemy(app)
 app.secret_key = "super_secret_key_123"  # 🔥 CRITICAL: Hardcoded secret
 email = "admin@abc.com"
 phone = "+921234567890"
+test = "92"
 ssn = "123-45-6789"
 test_access_key_id="ASIAYB2REOVTCGBPVKKJ"
 
